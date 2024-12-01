c408dc8df0a5568999a889202dddb3e0e21414b4
Marius Meyer <marius-meyer@hotmail.com>
2024-12-01 21:43:14 +0100

    Additional comments

--- src/WarehouseSim.jl
+++ src/WarehouseSim.jl
@@ -328,14 +328,36 @@ provided configuration for a given duration.
 - n: number of workers processing the items one at a time
 - duration: duration of the simulation
 
-All parameters can also be an `AbstractArray`. In that case, all combinations of
-input parameters will be simulated.
+All parameters can also be an `AbstractArray` or a range. In that case, 
+all combinations of input parameters will be simulated.
 Returns a DataFrame with the used configuration for each combination.
+
+Examples:
+
+Simulate a given configuration for different durations between 1000 and 5000 time units:
+
+    simulate_parametrized(1, 4, 1, 2, 10, 100, 8, 1000:1000:5000)
+
+Simulate a configuration with different λ_g given as array:
+
+    simulate_parametrized([1, 4, 6.3], 4, 1, 2, 10, 100, 8, 1000)
+
+If multiple input parameters :
+
+    simulate_parametrized([1, 2, 3], [1, 2, 3], 1, 2, 10, 100, 8, 1000)
+
+In this case, all combinations for the input parameters will be processed,
+resulting in a total of 6 simulation runs.
+
+
 """
 function simulate_parametrized(λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration)
     df = DataFrame()
+    # Lock to synchronize aggregation of partial results for each simulation run
     df_lock = Threads.Condition()
     # Get all combinations of input parameters
+    # and execute simulations over all available threads
+    # Use dynamic scheduling to improve load balancing
     Threads.@threads :dynamic for (λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration) in collect(Iterators.product(λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration))
         df_part = simulate_warehouse_queue(λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration)
         Threads.lock(df_lock)
