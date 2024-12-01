3715aa5858c7166ed93e971466b5a41a4c93c8b4
Marius Meyer <marius-meyer@hotmail.com>
2024-12-01 20:12:42 +0100

    Refactor ShipmentQueue and add basic threading

--- src/WarehouseSim.jl
+++ src/WarehouseSim.jl
@@ -6,7 +6,7 @@ using Distributions
 using Logging
 using ResumableFunctions
 using Statistics
-using Base: put!, take!, length
+using Base: iterate, put!, take!, length
 
 """
 Represents a shipment in the warehouse simulation.
@@ -84,19 +84,28 @@ mutable struct ShipmentQueue
 end
 
 """
-    is_full(q::ShipmentQueue) 
+    isfull(q::ShipmentQueue) 
 
 Returns true, if a ShipmentQueue is full because it reached `Q_max`
 shipments.
 """
-is_full(q::ShipmentQueue) = q.shipments.load >= q.Q_max
+isfull(q::ShipmentQueue) = q.shipments.load >= q.Q_max
 
-"""
-    is_empty(q::ShipmentQueue) 
-    
-Returns true, if a ShipmentQueue is empty.
-"""
-is_empty(q::ShipmentQueue) = q.shipments.load == 0
+function Base.iterate(q::ShipmentQueue)
+    d = iterate(q.shipments.items)
+    if !isnothing(d)
+        return first(d), last(d)
+    end
+    d
+end
+
+function Base.iterate(q::ShipmentQueue, state)
+    d = iterate(q.shipments.items, state)
+    if !isnothing(d)
+        return first(d), last(d)
+    end
+    d
+end
 
 """
     length(q::ShipmentQueue) 
@@ -112,14 +121,21 @@ Put a new shipment to the ShipmentQueue.
 Throws an error if the ShipmentQueue is full.
 """
 function Base.put!(q::ShipmentQueue, s::Shipment)
-    if !is_full(q)
-        if is_empty(q)
+    if !isfull(q)
+        if isempty(q)
+            @info "$(now(q.sim)) Something in there again"
             q.empty_time += now(q.sim) - q.empty_start_time
             q.empty_start_time = -1.0
         end
         put!(q.shipments, s)
+        if isfull(q) && q.full_start_time < 0
+            q.full_start_time = now(q.sim)
+        end
     else
-        q.full_start_time = now(q.sim)
+        if q.full_start_time < 0
+            q.full_start_time = now(q.sim)
+        end
+        put!(q.rejected_shipments, s)
         throw("Can't put $s. ShipmentQueue is full!")
     end
 end
@@ -132,12 +148,14 @@ If not shipment is available, it will block until a new
 shipment is put into the queue.
 """
 function Base.take!(q::ShipmentQueue)
-    if is_full(q)
+    if isfull(q)
         q.full_time += now(q.sim) - q.full_start_time
+        @info "$(now(q.sim)) Not full again $(q.full_time)"
         q.full_start_time = -1.0
     end
     s = take!(q.shipments)
-    if is_empty(q)
+    if isempty(q)
+        @info "$(now(q.sim)) Empty again"
         q.empty_start_time = now(q.sim)
     end
     s
@@ -200,13 +218,15 @@ warehouse queues.
         next_arrival = rand(Exponential(1 / queue.λ))
         @yield timeout(sim, next_arrival)
         new_shipment = Shipment(now(sim))
-        if !is_full(queue)
+        try
             @info "T $(now(sim)): $new_shipment arrived in $queue"
-            put!(queue.shipments, new_shipment)
+            put!(queue, new_shipment)
             put!(warehouse_state.avaliable_shipments, nothing)
-        else
-            @info "T $(now(sim)): $new_shipment rejected because $queue is full"
-            put!(queue.rejected_shipments, new_shipment)
+        catch e
+            @info "T $(now(sim)): $new_shipment rejected: $e"
+            if typeof(e) == MethodError
+                throw(e)
+            end
         end
     end
 end
@@ -226,13 +246,13 @@ Simulation process used to simulate worker behavior
         if (length(warehouse_state.grocery_queue) > length(warehouse_state.frozen_queue))
             worker.current_shipment = @yield take!(warehouse_state.grocery_queue)
             worker.current_shipment.start_processing = now(sim)
-            @yield timeout(sim, rand(Exponential(1 / p_g)))
+            @yield timeout(sim, rand(Exponential(p_g)))
             worker.current_shipment.end_processing = now(sim)
             put!(warehouse_state.processed_groceries, worker.current_shipment)
         else
             worker.current_shipment = @yield take!(warehouse_state.frozen_queue)
             worker.current_shipment.start_processing = now(sim)
-            @yield timeout(sim, rand(Exponential(1 / p_f)))
+            @yield timeout(sim, rand(Exponential(p_f)))
             worker.current_shipment.end_processing = now(sim)
             put!(warehouse_state.processed_frozen, worker.current_shipment)
         end
@@ -270,16 +290,62 @@ function simulate_warehouse_queue(λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration)
     DataFrame(Dict("λ_g" => [λ_g], "λ_f" => [λ_f], "Q_g" => [Q_g], "Q_f" => [Q_f], "p_g" => [p_g],
         "p_f" => [p_f], "n" => [n], "rejects_g" => [state.grocery_queue.rejected_shipments.load],
         "rejects_f" => [state.frozen_queue.rejected_shipments.load],
-        "worker_util" => [mean(w.working_time for w in workers) / duration],
-        "avg_wait_g" => [mean(s.start_processing - s.arrival_time for (s, _) in state.processed_groceries.items)],
-        "avg_wait_f" => [mean(s.start_processing - s.arrival_time for (s, _) in state.processed_frozen.items)],
+        "finished_g" => [state.processed_groceries.load],
+        "finished_f" => [state.processed_frozen.load], "duration" => [duration],
+        "worker_util" => [if !isempty(workers)
+            mean(w.working_time for w in workers) / duration
+        else
+            0
+        end],
+        "avg_wait_g" => [if !isempty(state.processed_groceries.items)
+            mean(s.start_processing - s.arrival_time for (s, _) in state.processed_groceries.items)
+        else
+            0.0
+        end],
+        "avg_wait_f" => [if !isempty(state.processed_frozen.items)
+            mean(s.start_processing - s.arrival_time for (s, _) in state.processed_frozen.items)
+        else
+            0.0
+        end],
         "full_rate_g" => [state.grocery_queue.full_time / duration],
         "full_rate_f" => [state.frozen_queue.full_time / duration],
         "empty_rate_g" => [state.grocery_queue.empty_time / duration],
         "empty_rate_f" => [state.frozen_queue.empty_time / duration]))
 end
 
+"""
+    simulate_parametrized(λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration)
+
+Simulate Warehouse queue management based on the
+provided configuration for a given duration.
+
+- λ_g: arrival rates for groceries
+- λ_f: arrival rate for frozen goods
+- p_g: processing times for groceries
+- p_f: processing times for frozen goods
+- Q_g: queue size for groceries
+- Q_f: queue size for frozen goods
+- n: number of workers processing the items one at a time
+- duration: duration of the simulation
+
+All parameters can also be an `AbstractArray`. In that case, all combinations of
+input parameters will be simulated.
+Returns a DataFrame with the used configuration for each combination.
+"""
+function simulate_parametrized(λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration)
+    df = DataFrame()
+    df_lock = Threads.Condition()
+    # Get all combinations of input parameters
+    Threads.@threads :dynamic for (λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration) in collect(Iterators.product(λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration))
+        df_part = simulate_warehouse_queue(λ_g, λ_f, p_g, p_f, Q_g, Q_f, n, duration)
+        Threads.lock(df_lock)
+        append!(df, df_part)
+        Threads.unlock(df_lock)
+    end
+    df
+end
+
 export simulate_warehouse_queue
-export is_full, is_empty, length, put!, take!
+export isfull, iterate, length, put!, take!
 export Worker, Shipment, ShipmentQueue, WarehouseState
 end
