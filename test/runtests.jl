ef62d83c779bd96a3daf5dd3c2b25d84bfed17aa
Marius Meyer <marius-meyer@hotmail.com>
2024-12-01 20:24:18 +0100

    Fix test description

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,35 +3,30 @@ using WarehouseSim
 using DataFrames
 using ConcurrentSim
 
-"""
-Test basic functionality of Shipment data structure
-"""
-@testset "Shipment" begin
+@testset "Test basic functionality of Shipment data structure" begin
     s = Shipment(2.0)
     @test s.arrival_time == 2.0
     @test s.start_processing == 0.0
     @test s.end_processing == 0.0
 end
 
-"""
-Test basic functionality of ShipmentQueue
-"""
-@testset "ShipmentQueue" begin
+
+@testset "Basic functionality ShipmentQueue" begin
     sim = Simulation()
     s = ShipmentQueue(sim, 2.0, 5)
     @test s.λ == 2.0
     @test s.Q_max == 5
     @test_throws InexactError ShipmentQueue(sim, 2.0, 3.5)
-    @test is_empty(s)
+    @test isempty(s)
     @test length(s) == 0
     shipment = Shipment(1)
     put!(s, shipment)
     for i in 2:5
-        @test is_full(s) == false
+        @test isfull(s) == false
         put!(s, Shipment(i))
-        @test is_empty(s) == false
+        @test isempty(s) == false
     end
-    @test is_full(s)
+    @test isfull(s)
     @test length(s) == 5
     @test_throws "ShipmentQueue is full!" put!(s, Shipment(6))
     @test length(s) == 5
@@ -39,11 +34,7 @@ Test basic functionality of ShipmentQueue
     @test length(s) == 4
 end
 
-"""
-Simple test run of simulation and basic sanity checks
-of output data.
-"""
-@testset "Simulation" begin
+@testset "Simple test run of simulation and basic sanity checks of output data." begin
     df = simulate_warehouse_queue(1, 1, 1, 1, 10, 10, 2, 20)
     @test typeof(df) == DataFrame
     @test first(df.n) == 2
